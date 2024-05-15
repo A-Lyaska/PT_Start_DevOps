781fe93fa77b481327defd801405a4c378f7a580
a_lyaska <2710747@mail.ru>
2024-05-15 18:01:24 +0300

    3.0

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+-- Создание пользователя для репликации
+CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'Qq123456';
+SELECT pg_create_physical_replication_slot('replication_slot');
+
+-- Создание базы данных devops, если она не существует
+SELECT 'CREATE DATABASE devops'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'devops')\gexec
+
+-- Создание таблиц для хранения email и телефонных номеров
+CREATE TABLE IF NOT EXISTS Emails (
+    ID SERIAL PRIMARY KEY,
+    Email VARCHAR (100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PhoneNums (
+    ID SERIAL PRIMARY KEY,
+    PhoneNum VARCHAR (100) NOT NULL
+);
+
+INSERT INTO emails (email) VALUES ('test@mail.ru');
+INSERT INTO phones (phone) VALUES ('8 987 65 43 21');
