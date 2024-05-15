e081e27abfb15acdc8c4e4f0221ef310e9de6389
a_lyaska <2710747@mail.ru>
2024-05-15 18:39:18 +0300

    3.2

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,9 @@
--- Создание пользователя для репликации
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'Qq123456';
 SELECT pg_create_physical_replication_slot('replication_slot');
 
--- Создание базы данных devops, если она не существует
 SELECT 'CREATE DATABASE devops'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'devops')\gexec
 
--- Создание таблиц для хранения email и телефонных номеров
 CREATE TABLE IF NOT EXISTS Emails (
     ID SERIAL PRIMARY KEY,
     Email VARCHAR (100) NOT NULL
